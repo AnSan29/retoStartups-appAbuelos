2572288f1d40df2a02eee8269e380b382572e3b1
Joseph Luis García Escobar <josephgarciaescobar597@gmail.com>
2025-07-23 17:58:24 -0500

    fix: Modificación de estilos en la vista de perfil de voluntarios

--- src/views/voluntarios/profile/profile.js
+++ src/views/voluntarios/profile/profile.js
@@ -1,7 +1,9 @@
 export default function VoluntarioProfile() {
   return `
-   <section class="voluntario-profile">
+  <section class="voluntario-profile">
     <div class="voluntario-card">
+      <h1>Bienvenido</h1>
+      <p><strong>Este es tu espacio donde podrás ver el estado de tus solicitudes, formularios, encuentas y lo más importante, tu perfil.</strong></p>
       <div class="voluntario-avatar">
         <img src="https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg?semt=ais_hybrid&w=740" alt="Foto de perfil del voluntario">
       </div>
@@ -12,10 +14,9 @@ export default function VoluntarioProfile() {
         <p><strong>Teléfono:</strong> <span class="voluntario-telefono"></span></p>
         <p><strong>Rol:</strong> <span class="voluntario-rol"></span></p>
       </div>
-
       <!-- Botón al final de la tarjeta -->
       <div class="voluntario-boton">
-        <button type="button"><a href="/dates" data-link>ayudar a un abuelito</a></button>
+        <button type="button"><a href="/dates" data-link>VER MIS CITAS</a></button>
       </div>
     </div>
   </section>
